7e81a464e686c734cc84505014f1eff82e8c442f
Yummy-Yums <eliasderby90@gmail.com>
2025-03-12 11:59:37 +0000

    done with primitives

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,7 +1,8 @@
 fn compute(a: u32, b: u32) -> u32 {
     // TODO: change the line below to fix the compiler error and make the tests pass.
     let multiplier: u8 = 4;
-    a + b * multiplier
+    let converted_to_: u32 = multiplier.into();
+    a + b * converted_to_
 }
 
 #[cfg(test)]
