cb5d2924b75f4e00d678a5c042c4d2e50d9e9a65
Yummy-Yums <eliasderby90@gmail.com>
2025-03-14 15:27:36 +0000

    finished traits

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,38 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+pub trait Power<Exponent = Self> {
+    type Output;
+
+    fn power(&self, num: Exponent) -> Self::Output;
+}
+
+impl Power<u16> for u32 {
+    type Output = u32;
+
+    fn power(&self, num: u16) -> Self::Output {
+        self.pow(num.into())
+    }
+}
+
+impl Power<&u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, num: &u32) -> Self::Output {
+        self.pow(*num)
+    }
+}
+
+impl Power<u32> for u32 {
+    type Output = u32;
+
+    fn power(&self, num: u32) -> Self::Output {
+        self.pow(num)
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::Power;
